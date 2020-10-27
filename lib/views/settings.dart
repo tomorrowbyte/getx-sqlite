1c5d98aeef3517a21bf10fc7aa25cad9be66edf8
Sebghat Yusuf <sebghatyusuf@gmail.com>
2020-10-27 16:45:35 +0430

    add new Image

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: CustomScrollView(
+      body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
-            actions: [],
-          )
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                "Settings".tr,
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+                
+              ),
+              background: Image.asset('assets/images/bg.jpg'),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.volume_off),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ],
       ),
     );
